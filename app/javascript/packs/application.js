49c8c4cdfc333f5da3eb921ace7dfdf2ac560ff9
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-07-10 06:18:26 +0000

    [mypage]レイアウト調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "../stylesheets/application"
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
