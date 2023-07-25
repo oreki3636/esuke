19864d12a8273363649dfd94362d7358c2238cb3
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-07-25 05:06:53 +0000

    デプロイエラー解決

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,6 +3,14 @@
 class Public::RegistrationsController < Devise::RegistrationsController
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
 
 
